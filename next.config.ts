b83b1f6d75df414f731576de3dbfd6bc03309b92
Tawiah Obed <tawiaho858@gmail.com>
2026-07-08 23:10:42 +0000

    added images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['172.20.10.2', 'localhost', '127.0.0.1'],
+ allowedDevOrigins: [
+  "localhost",
+  "127.0.0.1",
+  "172.20.10.3",
+  "30.30.100.163",
+],
   reactStrictMode: true,
   // Ensure no rewrites that capture /api/*
   async rewrites() {
